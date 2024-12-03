19b759b57f39dafad673c625338277dbb85bf1e3
Roberto Doto <rob22@live.it>
2024-12-03 14:09:52 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const sorry = document.getElementById('secret-p');
+const button = document.getElementById('showprojects');
+
+const showHiddenElement = (domELement) => {
+    if (domELement.style.display = 'none') {
+        domELement.style.display = 'block';
+    } else {
+        domELement.style.display = 'none';
+    }
+}
+
+
+button.addEventListener('click', () => {showHiddenElement(sorry)});
