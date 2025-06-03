a1490b746946fe7e41067a3db075c940afb8cde3
robertodoto <rob22@live.it>
2025-06-03 15:59:21 +0200

    fixed script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sorry = document.getElementById('secret-p');
 const button = document.getElementById('showprojects');
 
 const showHiddenElement = (domELement) => {
-    if (domELement.style.display = 'none') {
+    if (domELement.style.display === 'none') {
         domELement.style.display = 'block';
     } else {
         domELement.style.display = 'none';
